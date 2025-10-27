600f27d446eaad54670f82e85c2c1b7b3dbf9b8f
NemesisRoss <NemesisRoss@hotmail.fr>
2025-10-27 11:00:06 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from "./components/AppFooter.vue";
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <HelloWorld msg="ICI NOTRE PAGE D'ACCEUIL" />
+  <AppFooter></AppFooter>
+</template>
+
+<style scoped>
+
+</style>
